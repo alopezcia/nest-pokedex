ec8abc0a56fc315ce2ac6b6fcbeb02bfca5d530f
Adolfo Lopez <alopezcia@gmail.com>
2022-10-01 12:33:03 +0200

    Fin sección 7

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+
+    // id: String; // Mongo ya lo da
+    
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: String;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, HttpCode, HttpStatus } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+import { ParseMongoIdPipe } from '../common/pipes/parse-mongo-id.pipe';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  @HttpCode( HttpStatus.OK )   // Para personalizar el codigo devuelto para que en vez que salga 201, lo haga el 200 = OK
+  create(@Body() createPokemonDto: CreatePokemonDto) {
+    return this.pokemonService.create(createPokemonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Get(':term')
+  async findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
+  }
+
+  @Patch(':term')
+  update(@Param('term') term: string, @Body() updatePokemonDto: UpdatePokemonDto) {
+    return this.pokemonService.update(term, updatePokemonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseMongoIdPipe) id: string) {
+    return this.pokemonService.remove(id);
+  }
+}
